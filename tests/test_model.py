54c86e71cc33fb27d2d8152de1008e93cfc81717
gbruno16 <gius.bruno@yahoo.it>
2024-08-24 09:56:41 +0200

    Add skip test gencast full

--- tests/test_model.py
+++ tests/test_model.py
@@ -17,6 +17,7 @@ from graph_weather.models import (
 )
 from graph_weather.models.losses import NormalizedMSELoss
 
+import pytest
 from graph_weather.models.gencast.utils.noise import (
     generate_isotropic_noise,
     sample_noise_level,
@@ -464,7 +465,7 @@ def test_gencast_sampler():
     assert not torch.isnan(preds).any()
     assert preds.shape == (1, len(grid_lon), len(grid_lat), output_features_dim)
 
-
+@pytest.mark.skipif(torch.__version__ != "2.3.0", reason="dgl tests for experimental features only runs with torch 2.3.0")
 def test_gencast_full():
     # download weights from HF
     denoiser=Denoiser.from_pretrained("openclimatefix/gencast-128x64", 
